c4d9245229751e97c792c845f8622945856aca58
NeYru07 <146803248+NeYru07@users.noreply.github.com>
2024-10-24 17:50:12 +0400

    hakaton

---
+++ Учебная практика/Окно регистрации/regJS.js
@@ -0,0 +1,44 @@
+let btn = document.getElementById('btn');
+let inputs = document.querySelectorAll("input");
+let errorMessage = document.getElementById('error-message');
+
+// Кнопка регистрации
+btn.onclick = () => {
+	let password = document.getElementById('password');
+	let confirm = document.getElementById('confirm');
+	let email = document.getElementById('email').value.trim();
+	
+	// Сброс стилей
+	password.style.border = '';
+	confirm.style.border = '';
+	errorMessage.style.display = 'none';
+	errorMessage.textContent = '';
+
+	// Проверка на заполнение полей
+	let allFilled = Array.from(inputs).every(input => input.value.trim() !== "");
+
+	if (!allFilled) {
+		errorMessage.textContent = 'Пожалуйста, заполните все поля.';
+    	errorMessage.style.display = 'block';
+		return;
+	 }
+
+	//  Проверка на корректность Email
+	 const emailPattern = /^[^s@]+@[^s@]+.[^s@]+$/;
+	 if (!emailPattern.test(email)) {
+		errorMessage.textContent = 'Некорректный email адрес.';
+		errorMessage.style.display = 'block';
+		return;
+	 }
+	
+	// Проверка на совпадение паролей
+	if (password.value === confirm.value) {
+		location.replace("/Окно входа/entrance.html");
+	} else if (password.value !== confirm.value) {
+		password.style.border = '0.125rem solid red';
+		confirm.style.border = '0.125rem solid red';
+		errorMessage.textContent = 'Пароли не совпадают.';
+    	errorMessage.style.display = 'block';
+		showError();
+	}
+}
